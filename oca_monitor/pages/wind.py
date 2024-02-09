a6e25105c5e20028d5ef89172c55ce519ae10736
pwielgor <pwielgor@camk.edu.pl>
2024-02-09 07:27:04 +0100

    update wind

--- oca_monitor/pages/wind.py
+++ oca_monitor/pages/wind.py
@@ -39,15 +39,38 @@ class WindDataWidget(QWidget):
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
         self.ax_wind = self.figure.add_subplot(411)
-
+        self.ax_temp = self.figure.add_subplot(412)
+        self.ax_hum = self.figure.add_subplot(421)
+        self.ax_pres = self.figure.add_subplot(422)
+        self.ax_wind.set_title("Wind [m/s]")
+        self.ax_wind.set_title("Temperature [C]")
+        self.ax_wind.set_title("Humidity [%]")
+        self.ax_wind.set_title("Pressure [hPa]")
         # setting a limits
         self.ax_wind.set_xlim([0, 24])
         self.ax_wind.set_ylim([0, 20])
+        self.ax_temp.set_xlim([0, 24])
+        self.ax_hum.set_xlim([0, 24])
+        self.ax_hum.set_ylim([0, 90])
+        self.ax_pres.set_xlim([0, 24])
         x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
-        self.ax_wind.fill_between(x,10,13,color='orange',alpha=0.5)
-        self.ax_wind.fill_between(x,13,20,color='red',alpha=0.5)
-        self.ln_yesterday_wind = self.ax_wind.plot([],[], '.-', color='gray', label='Yesterday wind')[0]
-        self.ln_today_wind = self.ax_wind.plot([],[], '.-', color='blue', label='Today wind')[0]
+        self.ax_wind.fill_between(x,10,13,color='orange',alpha=0.3)
+        self.ax_wind.fill_between(x,13,20,color='red',alpha=0.3)
+
+        self.ax_hum.fill_between(x,70,80,color='orange',alpha=0.3)
+        self.ax_hum.fill_between(x,80,90,color='red',alpha=0.3)
+
+        self.ln_yesterday_wind = self.ax_wind.plot([],[], '.-', color='gray', alpha=0.3, label='Yesterday')[0]
+        self.ln_today_wind = self.ax_wind.plot([],[], '.-', color='blue', label='Today')[0]
+
+        self.ln_yesterday_temp = self.ax_temp.plot([],[], '.-', color='gray', alpha=0.3, label='Yesterday')[0]
+        self.ln_today_temp = self.ax_temp.plot([],[], '.-', color='blue', label='Today')[0]
+
+        self.ln_yesterday_hum = self.ax_hum.plot([],[], '.-', color='gray', alpha=0.3, label='Yesterday')[0]
+        self.ln_today_hum = self.ax_hum.plot([],[], '.-', color='blue', label='Today')[0]
+
+        self.ln_yesterday_pres = self.ax_pres.plot([],[], '.-', color='gray', alpha=0.3, label='Yesterday')[0]
+        self.ln_today_pres = self.ax_pres.plot([],[], '.-', color='blue', label='Today')[0]
         
         self.layout.addWidget(self.canvas)
 
@@ -65,7 +88,7 @@ class WindDataWidget(QWidget):
         )
         logger.info(f"Subscribed to {self.weather_subject}")
 
-        smaple_measurement = {
+        sample_measurement = {
                 "temperature_C": 10,
                 "humidity": 50,
                 "wind_dir_deg": 180,
@@ -91,10 +114,31 @@ class WindDataWidget(QWidget):
                 )
                 self.ln_today_wind.set_data([], [])
 
+                self.ln_yesterday_temp.set_data(
+                    self.ln_today_temp.get_xdata(),
+                    self.ln_today_temp.get_ydata()
+                )
+                self.ln_today_temp.set_data([], [])
+
+                self.ln_yesterday_hum.set_data(
+                    self.ln_today_hum.get_xdata(),
+                    self.ln_today_hum.get_ydata()
+                )
+                self.ln_today_hum.set_data([], [])
+
+                self.ln_yesterday_pres.set_data(
+                    self.ln_today_pres.get_xdata(),
+                    self.ln_today_pres.get_ydata()
+                )
+                self.ln_today_pres.set_data([], [])
+
             # handle current datapoint. it has measurement timestamp in data.ts, and the measurement in data.measurement
             ts = dt_ensure_datetime(data['ts']).astimezone()
             measurement = data['measurements']
             wind_speed10 = measurement['wind_10min_ms']
+            temp = measurement['temperature_c']
+            hum = measurement['humidity']
+            pres = measurement['pressure_Pa']/100.
             # depending on the date of the measurement, we want to add point to the yesterday or today data
             hour = ts.hour + ts.minute / 60 + ts.second / 3600
             if ts < today_midnight.astimezone():
@@ -103,15 +147,54 @@ class WindDataWidget(QWidget):
                     list(self.ln_yesterday_wind.get_xdata()) + [hour],
                     list(self.ln_yesterday_wind.get_ydata()) + [wind_speed10]
                 )
+
+                self.ln_yesterday_temp.set_data(
+                    list(self.ln_yesterday_temp.get_xdata()) + [hour],
+                    list(self.ln_yesterday_temp.get_ydata()) + [temp]
+                )
+
+                self.ln_yesterday_hum.set_data(
+                    list(self.ln_yesterday_hum.get_xdata()) + [hour],
+                    list(self.ln_yesterday_hum.get_ydata()) + [hum]
+                )
+
+                self.ln_yesterday_pres.set_data(
+                    list(self.ln_yesterday_pres.get_xdata()) + [hour],
+                    list(self.ln_yesterday_pres.get_ydata()) + [pres]
+                )
             else:
                 logger.info(f'Adding point to today data {wind_speed10}')
                 self.ln_today_wind.set_data(
                     list(self.ln_today_wind.get_xdata()) + [hour],
                     list(self.ln_today_wind.get_ydata()) + [wind_speed10]
                 )
+
+                self.ln_today_temp.set_data(
+                    list(self.ln_today_temp.get_xdata()) + [hour],
+                    list(self.ln_today_temp.get_ydata()) + [temp]
+                )
+
+                self.ln_today_wind.set_data(
+                    list(self.ln_todayt_hum.get_xdata()) + [hour],
+                    list(self.ln_today_hum.get_ydata()) + [hum]
+                )
+
+                self.ln_today_wind.set_data(
+                    list(self.ln_today_pres.get_xdata()) + [hour],
+                    list(self.ln_today_pres.get_ydata()) + [pres]
+                )
             # lazy redraw
             self.ax_wind.relim()
             self.ax_wind.autoscale_view()
+
+            self.ax_temp.relim()
+            self.ax_temp.autoscale_view()
+
+            self.ax_hum.relim()
+            self.ax_hum.autoscale_view()
+
+            self.ax_pres.relim()
+            self.ax_pres.autoscale_view()
             self.canvas.draw_idle()
 
 widget_class = WindDataWidget
