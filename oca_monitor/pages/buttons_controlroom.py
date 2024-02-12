d68990f842317b8874f4eabe34fd38ec8a809bab
pwielgor <pwielgor@camk.edu.pl>
2024-02-12 03:16:44 +0100

    update buttons controlroom

--- oca_monitor/pages/buttons_controlroom.py
+++ oca_monitor/pages/buttons_controlroom.py
@@ -63,7 +63,7 @@ class ButtonsWidgetControlroom(QWidget):
         self.lightSlides = []
         for i,light in enumerate(config.bbox_led_control_tel):
             self.lightSlides.append(lightSlide(light,config.bbox_led_control_tel[light],QCheckBox(light)))
-            self.lightSlides[-1].slide.setStyleSheet("QCheckBox::indicator:checked {image: url(./Icons/zb08_lightoff.png)}::indicator:unchecked {image: url(./Icons/zb08_lighton.png)}")
+            self.lightSlides[-1].slide.setStyleSheet("QCheckBox::indicator:checked {image: url(./Icons/zb08_lighton.png);height: 100px}::indicator:unchecked {image: url(./Icons/zb08_lightoff.png); height: 100px}")
             #.format('./Icons/'+light+"_lightoff.png",'./Icons/'+light+"_lighton.png"))
             self.lightSlides[-1].slide.setChecked(False)
             self.lightSlides[-1].slide.stateChanged.connect(self.lightSlides[-1].changeLight)
