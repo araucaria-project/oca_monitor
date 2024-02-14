15edfac78747a87b5b4847935066bd3b32aa34ed
pwielgor <pwielgor@camk.edu.pl>
2024-02-14 07:06:38 +0100

    update buttons controlroom and weather

--- oca_monitor/pages/buttons_controlroom.py
+++ oca_monitor/pages/buttons_controlroom.py
@@ -4,7 +4,9 @@ from PyQt6 import QtCore, QtGui
 import json,requests
 import oca_monitor.config as config
 from qasync import asyncSlot
+from serverish.base import dt_ensure_datetime
 from serverish.base.task_manager import create_task_sync, create_task
+from serverish.messenger import Messenger
 # please use logging like here, it will name the log record with the name of the module
 logger = logging.getLogger(__name__.rsplit('.')[-1])
 
@@ -56,6 +58,7 @@ class ButtonsWidgetControlroom(QWidget):
                  **kwargs  # other parameters
                  ):
         super().__init__()
+        
         self.initUI(example_parameter)
 
     def initUI(self, text):
@@ -114,7 +117,20 @@ class ButtonsWidgetControlroom(QWidget):
 
         # Some async operation
         self._update_lights_status()
+        QTimer.singleShot(0, self._update_warningWindow)
         logger.info("UI setup done")
+
+    @asyncSlot()
+    async def _update_warningWindow(self):
+        self.wind = '0.0'
+        self.temp = '0.0'
+        self.hum = '0.0'
+        self.pres = '0.0'
+        await create_task(self.reader_loop(), "weather reader")
+        warning = 'Wind: '+str(self.wind)+' m/n\n'+'Temperature: '+str(self.temp)+' C\n'+'Humidity: '+str(self.hum)+' %\n'
+        self.label.setText(warning)
+
+    
 
     def _update_lights_status(self):
         for light in self.lightSlides:
@@ -125,6 +141,42 @@ class ButtonsWidgetControlroom(QWidget):
                 light.slide.setStyleSheet("QCheckBox::indicator{width: 80px; height:70px;} QCheckBox::indicator:checked {image: url(./Icons/"+light.name+"_lightna.png)} QCheckBox::indicator:unchecked {image: url(./Icons/"+light.name+"_lightna.png)}")
 
         QtCore.QTimer.singleShot(30000, self._update_lights_status)
+
     
+    
+
+    async def reader_loop(self):
+        msg = Messenger()
+
+        # We want the data from the midnight of yesterday
+        
+
+        rdr = msg.get_reader(
+            self.weather_subject,
+            deliver_policy='last',
+        )
+        logger.info(f"Subscribed to {self.weather_subject}")
+
+        sample_measurement = {
+                "temperature_C": 10,
+                "humidity": 50,
+                "wind_dir_deg": 180,
+                "wind_ms": 5,
+                "wind_10min_ms": 5,
+                "pressure_Pa": 101325,
+                "bar_trend": 0,
+                "rain_mm": 0,
+                "rain_day_mm": 0,
+                "indoor_temperature_C": 20,
+                "indoor_humidity": 50,
+        }
+        async for data, meta in rdr:
+            hour = ts.hour + ts.minute / 60 + ts.second / 3600
+            measurement = data['measurements']
+            self.wind_speed10 = measurement['wind_10min_ms']
+            self.temp = measurement['temperature_C']
+            self.hum = measurement['humidity']
+            self.pres = measurement['pressure_Pa']
+
 
 widget_class = ButtonsWidgetControlroom
