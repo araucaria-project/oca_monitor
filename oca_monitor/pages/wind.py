7ffe53d45662f530282f118ba3f139fa8c11d6e0
pwielgor <pwielgor@camk.edu.pl>
2024-08-01 07:44:37 +0200

    update wind

--- oca_monitor/pages/wind.py
+++ oca_monitor/pages/wind.py
@@ -13,6 +13,29 @@ import numpy as np
 
 logger = logging.getLogger(__name__.rsplit('.')[-1])
 
+def ephemeris():
+    arm=ephem.Observer()
+    arm.pressure=730
+    #arm.horizon = '-0.5'
+    arm.lon='-70.201266'
+    arm.lat='-24.598616'
+    arm.elev=2800
+    arm.pressure=730
+    date = time.strftime('%Y%m%d',time.gmtime() )
+    ut = time.strftime('%Y/%m/%d %H:%M:%S',time.gmtime() )
+    t = czas_astro([ut.replace('/','-',2).replace(' ','T',1)])
+    lt = time.strftime('%Y/%m/%d %H:%M:%S',time.localtime() )
+    arm.date = ut
+    sunset=str(arm.next_setting(ephem.Sun()))
+    sunrise=str(arm.next_rising(ephem.Sun()))
+    sun = ephem.Sun()
+    sun.compute(arm)
+    
+    text = 'LT:\t'+lt+'\nSUN ALT:\t'+str(sun.alt)
+    
+    return text,sun.alt
+
+
 class WindDataWidget(QWidget):
     def __init__(self, main_window, subject='telemetry.weather.davis', vertical_screen = False, **kwargs):
         super().__init__()
@@ -35,7 +58,9 @@ class WindDataWidget(QWidget):
     def initUI(self):
         # Layout
         self.layout = QVBoxLayout(self)
-
+        self.label = QLabel("weather")
+        self.label.setStyleSheet("background-color : silver; color: black")
+        self.label.setFont(QtGui.QFont('Arial', 20))
         # Matplotlib setup
         self.figure = Figure(facecolor='lightgrey')
         self.canvas = FigureCanvas(self.figure)
@@ -84,7 +109,15 @@ class WindDataWidget(QWidget):
         self.ax_hum.grid(which='major', axis='both')
         self.ax_pres.grid(which='major', axis='both')
         self.figure.tight_layout()
-        self.layout.addWidget(self.canvas)
+        if self.vertical:
+            self.layout.addWidget(self.label)
+            self.layout.addWidget(self.canvas)
+        else:
+            hbox = self.QHBoxLayout(self)
+            hbox.addWidget(self.label)
+            hbox.addWidget(self.canvas)
+
+        QtCore.QTimer.singleShot(0, self._update_warningWindow)
 
     async def reader_loop(self):
         msg = Messenger()
@@ -211,5 +244,71 @@ class WindDataWidget(QWidget):
                 self.canvas.draw_idle()
             except:
                 pass
+
+    def _update_ephem(self):
+        self.ephem_text,sunalt = ephemeris()
+        self.sunalt = str(sunalt)
+
+    @asyncSlot()
+    async def _update_warningWindow(self):
+        self.wind = '0.0'
+        self.temp = '0.0'
+        self.hum = '0.0'
+        self.pres = '0.0'
+        await create_task(self.reader_loop(), "weather reader")
+        #warning = 'Wind: '+str(self.wind)+' m/s\n'+'Temperature: '+str(self.temp)+' C\n'+'Humidity: '+str(self.hum)+' %\n'+'Wind dir: '+str(self.main_window.winddir)+'\n'
+        #self.label.setText(warning)
+    
+
+    async def reader_loop(self):
+        msg = Messenger()
+
+        # We want the data from the midnight of yesterday
+        
+
+        rdr = msg.get_reader(
+            self.weather_subject,
+            deliver_policy='last',
+        )
+        logger.info(f"Subscribed to {self.weather_subject}")
+
+        sample_measurement = {
+                "temperature_C": 10,
+                "humidity": 50,
+                "wind_dir_deg": 180,
+                "wind_ms": 5,
+                "wind_10min_ms": 5,
+                "pressure_Pa": 101325,
+                "bar_trend": 0,
+                "rain_mm": 0,
+                "rain_day_mm": 0,
+                "indoor_temperature_C": 20,
+                "indoor_humidity": 50,
+        }
+        async for data, meta in rdr:
+            ts = dt_ensure_datetime(data['ts']).astimezone()
+            hour = ts.hour + ts.minute / 60 + ts.second / 3600
+            measurement = data['measurements']
+            self.wind = "{:.1f}".format(measurement['wind_10min_ms'])
+            self.temp = "{:.1f}".format(measurement['temperature_C'])
+            self.hum = int(measurement['humidity'])
+            self.pres = int(measurement['pressure_Pa'])
+            self.winddir = int(measurement['wind_dir_deg'])
+
+            self.main_window.wind = self.wind
+            self.main_window.temp = self.temp
+            self.main_window.hum = self.hum
+            self.main_window.winddir = self.winddir
+            self.main_window.skytemp = '0'
+
+            warning = self.text_ephem+'\n\nWind: '+str(self.wind)+' m/s\n'+'Temperature: '+str(self.temp)+' C\n'+'Humidity: '+str(self.hum)+' %\n'+'Wind dir: '+str(self.main_window.winddir)+'\n'
+            if (float(self.wind) >= 11. and float(self.wind) < 14.) or float(self.hum) > 70.:
+                self.label.setStyleSheet("background-color : yellow; color: black")
+            elif float(self.wind) >= 14. or float(self.hum) > 75.:
+                self.label.setStyleSheet("background-color : red; color: black")
+            else:
+                self.label.setStyleSheet("background-color : lightgreen; color: black")
+
+            self.label.setText(warning)
 
 widget_class = WindDataWidget
