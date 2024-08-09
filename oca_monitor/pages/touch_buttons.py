3e4165670f2b9feae104250bf2d414421c94afbb
pwielgor <pwielgor@camk.edu.pl>
2024-08-09 02:22:14 +0200

    update touch

--- oca_monitor/pages/touch_buttons.py
+++ oca_monitor/pages/touch_buttons.py
@@ -13,9 +13,6 @@ from astropy.time import Time as czas_astro
 # please use logging like here, it will name the log record with the name of the module
 logger = logging.getLogger(__name__.rsplit('.')[-1])
 
-def raise_alarm(mess):
-    pars = {'token':'adcte9qacd6jhmhch8dyw4e4ykuod2','user':'uacjyhka7d75k5i3gmfhdg9pc2vqyf','message':mess}
-    requests.post('https://api.pushover.net/1/messages.json',data=pars)
 
 
 
@@ -144,20 +141,23 @@ class TouchButtonsControlroom(QWidget):
 
         QtCore.QTimer.singleShot(15000, self._update_lights_status)
 
-    def send_alarm(self):
+    @asyncSlot()
+    async def send_alarm(self):
         if self.b_alarm.isChecked:
-            time.sleep(1)
-            raise_alarm('OCM: HELP!')
-            time.sleep(5)
+            await self.raise_alarm('OCM: HELP!')
+            time.sleep(3)
             self.b_alarm.setChecked(False)
 
-    def abort_observations(self):
+    @asyncSlot()
+    async def abort_observations(self):
         if self.b_abort.isChecked:
-            time.sleep(1)
-            raise_alarm('EMERGENCY STOP OBS!')
-            time.sleep(5)
+            await self.raise_alarm('EMERGENCY STOP OBS!')
+            time.sleep(3)
             self.b_abort.setChecked(False)
 
-   
+    async def raise_alarm(self,mess):
+        pars = {'token':'adcte9qacd6jhmhch8dyw4e4ykuod2','user':'uacjyhka7d75k5i3gmfhdg9pc2vqyf','message':mess}
+        requests.post('https://api.pushover.net/1/messages.json',data=pars)
+
 
 widget_class = TouchButtonsControlroom
