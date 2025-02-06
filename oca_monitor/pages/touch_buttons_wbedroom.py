165e8bb832659c47dcffbdb818bf1a419a18bfbe
pwielgor <pwielgor@camk.edu.pl>
2025-02-06 07:36:27 +0100

    update wbedroom

--- oca_monitor/pages/touch_buttons_wbedroom.py
+++ oca_monitor/pages/touch_buttons_wbedroom.py
@@ -223,10 +223,10 @@ class TouchButtonsWBedroom(QWidget):
         if wylacz:
             self.water_pump.button.setChecked(False)
         await self.changeWaterState()
-        #if self.water_pump.button.isChecked:
+        if self.water_pump.button.isChecked:
             #przycisk musi byc wlaczony przez okolo 2 sekundy zeby pompa sie uruchomila
             
-        #    QtCore.QTimer.singleShot(2000, lambda: self.water_button_pressed(wylacz=True))
+            QtCore.QTimer.singleShot(2000, lambda: self.water_button_pressed(wylacz=True))
 
     async def changeWaterState(self):
         self.water_pump.changeState()
