ad27e291186b730ccc63bd21cddbb35dc78782f6
pwielgor <pwielgor@camk.edu.pl>
2025-02-06 06:23:44 +0100

    update wbedroom, config

--- oca_monitor/config.py
+++ oca_monitor/config.py
@@ -22,6 +22,6 @@ bbox_blinds_control = {'bodega':'192.168.7.190','kitchen':'192.168.7.191'}
 bbox_bedroom_west = {'hot_water':'192.168.7.225'}
 
 #pushover user,token
-pushover = {'Piotrek':['uacjyhka7d75k5i3gmfhdg9pc2vqyf','adcte9qacd6jhmhch8dyw4e4ykuod2'],'Marek':['ugcgrfrrfn4eefnpiekgwqnxfwtrz5','adcte9qacd6jhmhch8dyw4e4ykuod2']}#,'weronika':[],'paulina':[],'mikolaj':[],'mirek':[],'bartek':[]}
+pushover = {'Piotrek':['uacjyhka7d75k5i3gmfhdg9pc2vqyf','adcte9qacd6jhmhch8dyw4e4ykuod2'],'Marek':['ugcgrfrrfn4eefnpiekgwqnxfwtrz5','adcte9qacd6jhmhch8dyw4e4ykuod2'],'OCMFON':['uqizn1afohvpeymtpu7cq67rabiidx','adcte9qacd6jhmhch8dyw4e4ykuod2']}#,'Weronika':[uqiowbkk91544jc5zab54vnsp5fjb2],'Paulina':[ucw5v3tyxwpes9e6wznf59f2s7hfzy],'Mikolaj':[],'Mirek':[],'Bartek':[]}
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
--- oca_monitor/pages/touch_buttons_wbedroom.py
+++ oca_monitor/pages/touch_buttons_wbedroom.py
@@ -13,11 +13,23 @@ from astropy.time import Time as czas_astro
 # please use logging like here, it will name the log record with the name of the module
 logger = logging.getLogger(__name__.rsplit('.')[-1])
 
+def ephemeris():
+    arm=ephem.Observer()
+    arm.pressure=730
+    arm.lon='-70.201266'
+    arm.lat='-24.598616'
+    arm.elev=2800
+    arm.pressure=730
+    lt = time.strftime('%Y/%m/%d %H:%M:%S',time.localtime() )
+    sun = ephem.Sun()
+    sun.compute(arm)
+    return lt,sun.alt
+
 class bboxItem():
     def __init__(self,name,ip,slide):
         self.name = name
         self.ip = ip
-        self.slide = slide
+        self.button = button
 
     def is_avilable(self):
         try:
@@ -34,7 +46,7 @@ class bboxItem():
         if self.is_active:
             #try:
             if True:
-                if self.slide.isChecked():
+                if self.button.isChecked():
                     value = 1
                 else:
                     value = 0
@@ -47,7 +59,7 @@ class bboxItem():
         requests.post('http://'+self.ip+'/state',json={"relays":[{"relay":0,"state":val}]})
 
 
-class lightSlide():
+'''class lightSlide():
     def __init__(self,name,ip,slide):
         self.name = name
         self.ip = ip
@@ -130,7 +142,7 @@ class light_point():
                 self.curr_value = int(req.json()["rgbw"]["desiredColor"],16)
                 self.slider.setValue(int(self.curr_value*100/255))
         except:
-            self.is_active = False
+            self.is_active = False'''
 
 
 class TouchButtonsWBedroom(QWidget):
@@ -139,110 +151,160 @@ class TouchButtonsWBedroom(QWidget):
                  main_window, # always passed
                  example_parameter: str = "Hello OCM!",  # parameters from settings
                  subject='telemetry.weather.davis',#weather subject
+                 temp_subject='telemetry.conditions.bedroom-west-tsensor',
                  **kwargs  # other parameters
                  ):
         super().__init__()
         self.main_window = main_window
+        self.weather_subject = subject
+        self.temp_subject = temp_subject
         self.initUI(example_parameter,subject)
 
     def initUI(self, text,subject):
         
         self.layout = QHBoxLayout(self)
-        self.b_abort = QCheckBox()#abort button
-        self.b_abort.setStyleSheet("QCheckBox::indicator{width: 300px; height:300px;} QCheckBox::indicator:checked {image: url(./Icons/closedomeson.png)} QCheckBox::indicator:unchecked {image: url(./Icons/closedomesoff.png)}")
-        self.b_abort.stateChanged.connect(self.abort_observations)
+        self.vbox_left = QVBoxLayout()
+        self.vbox_center = QVBoxLayout()
+        self.vbox_right = QVBoxLayout()
         
+        self.label_ephem = QLabel("ephem")
+        self.label_ephem.setStyleSheet("background-color : gray; color: white")
+        self.label_ephem.setFont(QtGui.QFont('Arial', 26))
+
+        self.vbox_left.addWidget(self.label_ephem)
+
+        self.label_weather = QLabel("weather")
+        self.label_weather.setStyleSheet("background-color : silver; color: black")
+        self.label_weather.setFont(QtGui.QFont('Arial', 26))
+
+        self.vbox_center.addWidget(self.label_weather)
+
         self.b_alarm = QCheckBox()#abort button
         self.b_alarm.setStyleSheet("QCheckBox::indicator{width: 300px; height:300px;} QCheckBox::indicator:checked {image: url(./Icons/alarmon.png)} QCheckBox::indicator:unchecked {image: url(./Icons/alarmoff.png)}")
         self.b_alarm.stateChanged.connect(self.send_alarm)
 
-        self.enable_abort = QCheckBox('Enable close button')
-        self.enable_abort.setStyleSheet("QCheckBox::indicator{width: 120px; height:80px;} QCheckBox::indicator:checked {image: url(./Icons/SwitchOn.png)} QCheckBox::indicator:unchecked {image: url(./Icons/SwitchOff.png)}")
-
-
-        self.vbox_enable_buttons = QVBoxLayout()
-        
-        
-
-        self.vbox_emergency_buttons = QVBoxLayout()
-        self.vbox_emergency_buttons.addWidget(self.b_abort)
-        self.vbox_emergency_buttons.addWidget(self.b_alarm)
+        self.vbox_right.addWidget(self.b_alarm)
                 
-        self.vbox_light_buttons_left = QVBoxLayout()
-        self.vbox_light_buttons_right = QVBoxLayout()
-        self.hbox_light_buttons = QHBoxLayout()
-        #self.vbox_light_buttons_left.addWidget(self.label_lights)
+        self.water_pump=bboxItem(item,config.bbox_bedroom_west[item],QCheckBox())
+        self.water_pump.button.setStyleSheet("QCheckBox::indicator{width: 200px; height:200px;} QCheckBox::indicator:checked {image: url(./Icons/"+item+"_on.png)} QCheckBox::indicator:unchecked {image: url(./Icons/"+item+"_off.png)}")
+        self.water_pump.button.setChecked(False)
+        self.water_pump.button.stateChanged.connect(self.bedroomStaff[-1].changeState)
+        self.vbox_left.addWidget(self.water_pump.button)
 
-        self.lights = []
-        
-        for i,light in enumerate(config.bbox_led_control_controlroom):
-            #self.lights.append(light_point(light,config.bbox_led_control[light],QPushButton('+'),QPushButton('-'),QLabel('LIGHT '+light)))
-            self.lights.append(light_point(light,config.bbox_led_control_controlroom[light],QDial()))
-            self.vbox_enable_buttons.addWidget(self.lights[-1].slider,1)
-
-        self.bedroomStaff = []
-        for i,item in enumerate(config.bbox_bedroom_west):
-            self.bedroomStaff.append(bboxItem(item,config.bbox_bedroom_west[item],QCheckBox()))
-            self.bedroomStaff[-1].slide.setStyleSheet("QCheckBox::indicator{width: 200px; height:200px;} QCheckBox::indicator:checked {image: url(./Icons/"+item+"_on.png)} QCheckBox::indicator:unchecked {image: url(./Icons/"+item+"_off.png)}")
-            self.bedroomStaff[-1].slide.setChecked(False)
-            self.bedroomStaff[-1].slide.stateChanged.connect(self.bedroomStaff[-1].changeState)
-
-            if i%2==1:
-                self.vbox_light_buttons_right.addWidget(self.bedroomStaff[-1].slide)
-            else:
-                self.vbox_light_buttons_left.addWidget(self.bedroomStaff[-1].slide)
-
-        self.hbox_light_buttons.addLayout(self.vbox_light_buttons_left)
-        self.hbox_light_buttons.addLayout(self.vbox_light_buttons_right)
-
-        self.hbox_main = QHBoxLayout()
-        self.hbox_main.addLayout(self.hbox_light_buttons)
-        self.hbox_main.addLayout(self.vbox_enable_buttons)
-        self.hbox_main.addLayout(self.vbox_emergency_buttons)
-        self.layout.addLayout(self.hbox_main)
+    
+        self.layout.addLayout(self.vbox_left)
+        self.layout.addLayout(self.vbox_center)
+        self.layout.addLayout(self.vbox_right)
 
         # Some async operation
         
-        self._update_lights_status()
+        QtCore.QTimer.singleShot(1000, self._update_weather)
+        QtCore.QTimer.singleShot(1000, self._update_ephem)
         
         logger.info("UI setup done")
 
     
-    def _update_lights_status(self):
-        for light in self.bedroomStaff:
-            light.is_avilable()
-            if light.is_active:
-                light.slide.setStyleSheet("QCheckBox::indicator{width: 200px; height:200px;} QCheckBox::indicator:checked {image: url(./Icons/"+light.name+"_on.png)} QCheckBox::indicator:unchecked {image: url(./Icons/"+light.name+"_off.png)}")
-            #else:
-            #    light.slide.setStyleSheet("QCheckBox::indicator{width: 200px; height:200px;} QCheckBox::indicator:checked {image: url(./Icons/"+light.name+"_na.png)} QCheckBox::indicator:unchecked {image: url(./Icons/"+light.name+"_na.png)}")
-
-        for light in self.lights:
-            light.status()
-            
-
-        QtCore.QTimer.singleShot(15000, self._update_lights_status)
-
-
     @asyncSlot()
-    async def send_alarm(self):
-        if self.b_alarm.isChecked:
-            await self.raise_alarm('OCM HELP!')
-
-        self.b_alarm.setChecked(False)
-
-    @asyncSlot()
-    async def abort_observations(self):
+    async def button_pressed(self):
         if self.b_abort.isChecked:
             await self.raise_alarm('EMERGENCY STOP OBS!')
             
         self.b_abort.setChecked(False)
 
+    @asyncSlot()
+    async def send_alarm(self):
+        if self.b_alarm.isChecked:
+            await self.raise_alarm('OCM: HELP US, ')
+
+        self.b_alarm.setChecked(False)
+
+
     async def raise_alarm(self,mess):
         for name,po_data in config.pushover.items():
             user = po_data[0]
             token = po_data[1]
-            pars = {'token':token,'user':user,'message':name+' '+mess}
+            pars = {'token':token,'user':user,'message':mess+name+'!'}
             requests.post('https://api.pushover.net/1/messages.json',data=pars)
 
+    def _update_ephem(self):
+        lt,sunalt = ephemeris()
+        sunalt = str(sunalt)
+        text = str(lt)+'\nSUN ALT: '+sunalt
+        self.label_ephem.setText(text)
+        
+        QtCore.QTimer.singleShot(1000, self._update_ephem)
+
+    @asyncSlot()
+    async def _update_warningWindow(self):
+        self.wind = '0.0'
+        self.temp = '0.0'
+        self.hum = '0.0'
+        self.pres = '0.0'
+        await create_task(self.reader_loop_2(), "weather reader")
+        #warning = 'Wind: '+str(self.wind)+' m/s\n'+'Temperature: '+str(self.temp)+' C\n'+'Humidity: '+str(self.hum)+' %\n'+'Wind dir: '+str(self.main_window.winddir)+'\n'
+        #self.label.setText(warning)
+    
+
+    async def reader_loop_2(self):
+        
+        msg = Messenger()
+
+        # We want the data from the midnight of yesterday
+        
+
+        rdr = msg.get_reader(
+            self.weather_subject,
+            deliver_policy='last',
+        )
+        logger.info(f"Subscribed to {self.weather_subject}")
+
+        sample_measurement = {
+                "temperature_C": 10,
+                "humidity": 50,
+                "wind_dir_deg": 180,
+                "wind_ms": 5,
+                "wind_10min_ms": 5,
+                "pressure_Pa": 101325,
+                "bar_trend": 0,
+                "rain_mm": 0,
+                "rain_day_mm": 0,
+                "indoor_temperature_C": 20,
+                "indoor_humidity": 50,
+        }
+        async for data, meta in rdr:
+            ts = dt_ensure_datetime(data['ts']).astimezone()
+            hour = ts.hour + ts.minute / 60 + ts.second / 3600
+            measurement = data['measurements']
+            self.wind = "{:.1f}".format(measurement['wind_10min_ms'])
+            self.temp = "{:.1f}".format(measurement['temperature_C'])
+            self.hum = int(measurement['humidity'])
+            self.pres = int(measurement['pressure_Pa'])
+            self.winddir = int(measurement['wind_dir_deg'])
+
+            self.main_window.wind = self.wind
+            self.main_window.temp = self.temp
+            self.main_window.hum = self.hum
+            self.main_window.winddir = self.winddir
+            self.main_window.skytemp = '0'
+            if self.vertical:
+                warning = 'Wind:\t'+str(self.wind)+' m/s\n'+'Temp:\t'+str(self.temp)+' C\n'+'Hum:\t'+str(self.hum)+' %\n'+'Wdir:\t'+str(self.main_window.winddir)+' deg'
+            else:
+                warning = '   Wind:\t\t'+str(self.wind)+' m/s\n'+'   Temperature:\t'+str(self.temp)+' C\n'+'   Humidity:\t'+str(self.hum)+' %\n'+'   Wind dir:\t'+str(self.main_window.winddir)+' deg'
+            if (float(self.wind) >= 11. and float(self.wind) < 14.) or float(self.hum) > 70.:
+                self.label.setStyleSheet("background-color : yellow; color: black")
+                if self.main_window.sound_page:
+                    self.main_window.sound_page.play_weather_warning(True)
+            elif float(self.wind) >= 14. or float(self.hum) > 75. or float(self.temp) < 0.:
+                self.label.setStyleSheet("background-color : red; color: black")
+                if self.main_window.sound_page:
+                    self.main_window.sound_page.play_weather_warning(False)
+                    self.main_window.sound_page.play_weather_stop(True)
+            else:
+                if self.main_window.sound_page:
+                    self.main_window.sound_page.play_weather_warning(False)
+                    self.main_window.sound_page.play_weather_stop(False)
+                self.label.setStyleSheet("background-color : lightgreen; color: black")
+
+            self.label.setText(warning)
 
 widget_class = TouchButtonsWBedroom
