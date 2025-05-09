f041dec5e4cddb4a42b06d862d739479aa0c7c3b
Mmiirrkk <cot@wp.pl>
2025-05-09 15:24:58 +0200

    goes satelite 11d

--- oca_monitor/pages/satellite_animation.py
+++ oca_monitor/pages/satellite_animation.py
@@ -27,7 +27,7 @@ class SatelliteAnimationWidget(QWidget):
 
     IMAGE_PREFIX = '600x600'
     REFRESH_IMAGE_TIME_SEC = 10
-    IMAGE_CHANGE_SEC = 0.5
+    IMAGE_CHANGE_SEC = 0.75
     MAX_IMAGES_NO = 12
 
     def __init__(self, main_window, allsky_dir='/data/misc/GOES_satellite/', vertical_screen = False, **kwargs):
@@ -56,67 +56,67 @@ class SatelliteAnimationWidget(QWidget):
         QTimer.singleShot(0, self.async_init)
         # self.update_v2()
         
-    def update(self):
-
-
-        try:
-            lista = os.popen('ls -tr '+self.dir+'*600x600.jpg').read().split('\n')[:-1]
-            if len(lista) > 4:
-                lista = lista[-4:]
-
-            if len(lista)> 0:
-                figure = QPixmap(lista[self.counter])
-
-                if self.vertical:
-                    self.label.setPixmap(figure.scaled(self.height(),self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
-                else:
-                    self.label.setPixmap(figure.scaled(self.height(), self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
-
-                self.counter = self.counter + 1
-                if self.counter == len(lista):
-                    self.counter = 0
-        except:
-            self.counter = 0
-
-        QTimer.singleShot(self.freq, self.update)
-        self._change_update_time()
-
-
-    def update_v2(self):
-        files_list = []
-        try:
-            files_found = os.listdir(self.dir)
-        except OSError:
-            logger.error(f'Can not access {self.dir}.')
-            files_found = []
-
-        for file in files_found:
-            if self.IMAGE_PREFIX in file:
-                files_list.append(file)
-
-        if len(files_list) == 0:
-            logger.warning(f'No files.')
-
-        else:
-            files_list.sort()
-            lista = [os.path.join(self.dir, f) for f in files_list]
-            if len(lista) > 4:
-                lista = lista[-4:]
-
-            if len(lista) > 0:
-                figure = QPixmap(lista[self.counter])
-
-                if self.vertical:
-                    self.label.setPixmap(figure.scaled(self.height(),self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
-                else:
-                    self.label.setPixmap(figure.scaled(self.height(), self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
-
-                self.counter = self.counter + 1
-                if self.counter == len(lista):
-                    self.counter = 0
-
-        QTimer.singleShot(self.freq, self.update_v2)
-        self._change_update_time()
+    # def update(self):
+    #
+    #
+    #     try:
+    #         lista = os.popen('ls -tr '+self.dir+'*600x600.jpg').read().split('\n')[:-1]
+    #         if len(lista) > 4:
+    #             lista = lista[-4:]
+    #
+    #         if len(lista)> 0:
+    #             figure = QPixmap(lista[self.counter])
+    #
+    #             if self.vertical:
+    #                 self.label.setPixmap(figure.scaled(self.height(),self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
+    #             else:
+    #                 self.label.setPixmap(figure.scaled(self.height(), self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
+    #
+    #             self.counter = self.counter + 1
+    #             if self.counter == len(lista):
+    #                 self.counter = 0
+    #     except:
+    #         self.counter = 0
+    #
+    #     QTimer.singleShot(self.freq, self.update)
+    #     self._change_update_time()
+    #
+    #
+    # def update_v2(self):
+    #     files_list = []
+    #     try:
+    #         files_found = os.listdir(self.dir)
+    #     except OSError:
+    #         logger.error(f'Can not access {self.dir}.')
+    #         files_found = []
+    #
+    #     for file in files_found:
+    #         if self.IMAGE_PREFIX in file:
+    #             files_list.append(file)
+    #
+    #     if len(files_list) == 0:
+    #         logger.warning(f'No files.')
+    #
+    #     else:
+    #         files_list.sort()
+    #         lista = [os.path.join(self.dir, f) for f in files_list]
+    #         if len(lista) > 4:
+    #             lista = lista[-4:]
+    #
+    #         if len(lista) > 0:
+    #             figure = QPixmap(lista[self.counter])
+    #
+    #             if self.vertical:
+    #                 self.label.setPixmap(figure.scaled(self.height(),self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
+    #             else:
+    #                 self.label.setPixmap(figure.scaled(self.height(), self.height(), QtCore.Qt.AspectRatioMode.KeepAspectRatio))
+    #
+    #             self.counter = self.counter + 1
+    #             if self.counter == len(lista):
+    #                 self.counter = 0
+    #
+    #     QTimer.singleShot(self.freq, self.update_v2)
+    #     self._change_update_time()
 
     async def a_image_list_refresh(self):
         while True:
@@ -171,7 +171,7 @@ class SatelliteAnimationWidget(QWidget):
         await create_task(self.a_image_list_refresh(), 'satellite_refresh_images')
         await create_task(self.a_display(), 'satellite_display_images')
 
-    def _change_update_time(self):
-        self.freq = 1000
+    # def _change_update_time(self):
+    #     self.freq = 1000
 
 widget_class = SatelliteAnimationWidget
