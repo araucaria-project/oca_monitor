49d21960b9142b2401ae03626ea7ab7e67c32b29
Mmiirrkk <cot@wp.pl>
2025-05-09 16:48:08 +0200

    goes satelite 18d

--- image_display.py
+++ image_display.py
@@ -52,6 +52,7 @@ class ImageDisplay:
                             if self.image_queue.qsize() > len(current_files_list_path) - 1:
                                 _ = await self.image_queue.get()
                             await self.image_queue.put(await image_instance_clb(image_path=new_file))
+                        print(self.image_queue)
                 logger.info(f'{self.name} files list updated by new files no: {new_files_no}.')
             await asyncio.sleep(self.refresh_image_time_sec)
 
