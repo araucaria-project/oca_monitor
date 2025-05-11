d1f353c520d9bfd963778546f60260668ee79f69
MMiirrkk <cot@wp.pl>
2025-05-11 10:30:38 +0200

    changed refresh 1

--- image_display.py
+++ image_display.py
@@ -3,6 +3,7 @@ import copy
 import logging
 import os
 import time
+from typing import List
 
 from serverish.base.task_manager import create_task
 
@@ -12,23 +13,41 @@ logger = logging.getLogger(__name__.rsplit('.')[-1])
 
 class ImageDisplay:
 
-    MODES = {'new_files': {}, 'modify_time': {}}
+    MODES = {'new_files': {}, 'update_files': {}}
 
     def __init__(
-            self, name: str, images_dir: str, images_prefix: str = '',
-            image_change_sec: float = 0.75, refresh_image_time_sec: float = 10, mode: str = 'new_files') -> None:
+            self, name: str, images_dir: str, image_display_clb: callable, image_instance_clb: callable,
+            images_prefix: str = '', image_change_sec: float = 0.75, image_pause_sec: float = 1.5,
+            refresh_image_sec: float = 10, mode: str = 'new_files') -> None:
         self.name = name
         self.images_dir = images_dir
         self.image_queue = asyncio.Queue()
-        self.files_list = []
+        self.image_display_clb = image_display_clb
+        self.image_instance_clb = image_instance_clb
         self.images_prefix = images_prefix
         self.image_change_sec = image_change_sec
-        self.refresh_image_time_sec = refresh_image_time_sec
+        self.refresh_image_sec = refresh_image_sec
+        self.image_pause_sec = image_pause_sec
         self.last_refresh = None
+        self.mode = mode
         super().__init__()
 
-    async def image_list_refresh(self, image_instance_clb: callable):
-        # while True:
+    async def new_files_refresh(self, files_list: List):
+        logger.info(f'{self.name} files list updating...')
+        current_images = []
+        for n in range(self.image_queue.qsize()):
+            image_queue = await self.image_queue.get()
+            if image_queue[0] in files_list:
+                await self.image_queue.put(image_queue)
+                current_images.append(image_queue[0])
+        new_files = [x for x in files_list if x not in current_images]
+        new_files_no = len(new_files)
+        if new_files_no > 0:
+            for new_file in new_files:
+                await self.image_queue.put((new_file, await self.image_instance_clb(image_path=new_file)))
+        logger.info(f'{self.name} files list updated by new files no: {new_files_no}.')
+
+    async def image_list_refresh(self):
         current_files_list = []
         try:
             files_found = os.listdir(self.images_dir)
@@ -41,36 +60,27 @@ class ImageDisplay:
                 current_files_list.append(file)
         current_files_list_path = [os.path.join(self.images_dir, f) for f in current_files_list]
         current_files_list_path.sort()
-        if current_files_list_path != self.files_list:
-            logger.info(f'{self.name} files list updating...')
-            new_files = [x for x in current_files_list_path if x not in self.files_list]
-            new_files_no = len(new_files)
-            if new_files_no > 0:
-                self.files_list = copy.deepcopy(current_files_list_path)
-                for new_file in new_files:
-                    if self.image_queue.qsize() > len(current_files_list_path) - 1:
-                        _ = await self.image_queue.get()
-                    await self.image_queue.put(await image_instance_clb(image_path=new_file))
-            logger.info(f'{self.name} files list updated by new files no: {new_files_no}.')
-            # await asyncio.sleep(self.refresh_image_time_sec)
+        if self.mode in self.MODES:
+            if self.mode == 'new_files':
+                await self.new_files_refresh(files_list=current_files_list_path)
+            elif self.mode == 'update_files':
+                raise NotImplementedError
+        else:
+            logger.error(f'No mode in modes, select: {self.MODES}.')
 
-    async def display(self, image_display_clb: callable, image_instance_clb: callable) -> None:
+    async def display(self) -> None:
         while True:
-
             for n in range(self.image_queue.qsize()):
                 if self.image_queue.qsize() > 0:
                     image_to_display = await self.image_queue.get()
-                    await image_display_clb(image_to_display=image_to_display)
+                    await self.image_display_clb(image_to_display=image_to_display[1])
                     await self.image_queue.put(image_to_display)
                 await asyncio.sleep(self.image_change_sec)
-            if not self.last_refresh or time.time() > self.last_refresh + self.refresh_image_time_sec:
-                await self.image_list_refresh(image_instance_clb=image_instance_clb)
+            if not self.last_refresh or time.time() > self.last_refresh + self.refresh_image_sec:
+                await self.image_list_refresh()
                 self.last_refresh = time.time()
             await asyncio.sleep(self.image_change_sec)
 
     async def display_init(self, image_display_clb: callable, image_instance_clb: callable):
         logger.info(f'Starting {self.name} display.')
-        # await create_task(
-        #     self.image_list_refresh(image_instance_clb=image_instance_clb), f'{self.name}_refresh_images'
-        # )
-        await create_task(self.display(image_display_clb=image_display_clb, image_instance_clb=image_instance_clb), f'{self.name}_display_images')
+        await create_task(self.display(), f'{self.name}_display_images')
