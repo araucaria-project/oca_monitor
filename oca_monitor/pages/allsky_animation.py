22df7b0788411309a1b8445a138cb21acf1cf904
Mmiirrkk <cot@wp.pl>
2025-05-13 11:13:23 +0200

    ver 4b

--- oca_monitor/pages/allsky_animation.py
+++ oca_monitor/pages/allsky_animation.py
@@ -58,7 +58,7 @@ class AllskyAnimationWidget(QWidget):
         display = ImageDisplay(
             name='allsky', images_dir=self.dir, image_display_clb=self.image_display,
             image_instance_clb=self.image_instance, images_prefix = 'lastimage',
-            image_cascade_sec = 0.75, image_pause_sec=1.25, refresh_list_sec = 10, mode='new_files'
+            image_cascade_sec = 0.75, image_pause_sec=1.25, refresh_list_sec = 10, mode='new_files', sort_reverse=True
         )
         await display.display_init()
 
