34847548a5a5d45ac73eae05795f6032b36a7b69
Mmiirrkk <cot@wp.pl>
2025-05-13 09:04:47 +0200

    refactor telescope ofp 2

--- oca_monitor/pages/wk06_ofp.py
+++ oca_monitor/pages/wk06_ofp.py
@@ -9,6 +9,6 @@ logger = logging.getLogger(__name__.rsplit('.')[-1])
 class WidgetTvsControlroom(TelescopeOfp):
 
     def __init__(self, main_window, **kwargs):
-        super().__init__(main_window=main_window, tel='zb08', kwargs=kwargs)
+        super().__init__(main_window=main_window, tel='wk06', kwargs=kwargs)
 
 widget_class = WidgetTvsControlroom
