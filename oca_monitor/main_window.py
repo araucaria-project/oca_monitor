42f127af8e1b63c47f51ee1537560664fc9a9506
MMiirrkk <cot@wp.pl>
2025-08-27 21:59:44 +0200

    add task checker 1a

--- oca_monitor/main_window.py
+++ oca_monitor/main_window.py
@@ -243,7 +243,7 @@ class MainWindow(QMainWindow):
 
     _config_reading_in_progress = Lock()
 
-    async def run_task_monitor(self, period: float = 5) -> None:
+    async def run_task_monitor(self, period: float = 60) -> None:
 
         while True:
             checked_tasks = 0
