99ba6fa44b14ac63c5af3189df68a729d0f90bd6
MMiirrkk <cot@wp.pl>
2025-08-29 19:53:39 +0200

    refactor conditions 1

--- oca_monitor/pages/conditions_screens.py
+++ oca_monitor/pages/conditions_screens.py
@@ -1,25 +1,20 @@
-import asyncio
 import logging
 import datetime
-from typing import Dict
-
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout,QHBoxLayout, QLabel
 from PyQt6.QtCore import QTimer
 from PyQt6 import QtGui
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from qasync import asyncSlot
 from serverish.base import dt_ensure_datetime
 from serverish.base.task_manager import create_task_sync, create_task
 from serverish.messenger import Messenger
-import numpy as np
-
 import oca_monitor.config as config
 
 
 logger = logging.getLogger(__name__.rsplit('.')[-1])
+
 
 class sensor():
     def __init__(self,name,name_to_display='',x=0,y=0):
@@ -29,6 +24,7 @@ class sensor():
         self.hum = "Undef"
         self.x = x
         self.y = y
+
 
 class ConditionsScreensWidget(QWidget):
     def __init__(self, main_window, subject_conditions='telemetry.conditions', subject_water='telemetry.water.level', subject_energy='telemetry.power.data-manager', vertical_screen = True, **kwargs):
@@ -40,26 +36,20 @@ class ConditionsScreensWidget(QWidget):
         self.vertical = bool(vertical_screen)
         self.sensors = {}
         self.htsensors = config.ht_subjects
-        # self.locks: Dict[asyncio.Lock] = {}
         self.initUI()
         QTimer.singleShot(0, self.async_init)
         # async init
-        
 
     @asyncSlot()
     async def async_init(self):
-        #obs_config = await self.main_window.observatory_config()
         await create_task(self.reader_loop_water(), "nats_reader_water")
         await create_task(self.reader_loop_energy(), "nats_reader_energy")
         for sens, params in self.htsensors.items():
             if sens not in self.sensors.keys():
-
                 self.sensors[sens]=sensor(sens,params[0],x=params[1],y=params[2])
                 logger.info(sens,params[1],params[2])
-            # self.locks[sens] = asyncio.Lock()
             subject = self.subject_conditions+'.'+sens
             await create_task(self.reader_loop_conditions(subject,sens), f"nats_reader_conditions_{sens}")
-
 
     def initUI(self):
         # Layout
@@ -97,7 +87,6 @@ class ConditionsScreensWidget(QWidget):
         async for data, meta in rdr:
             # async with self.locks[sens]:
             try:
-                self.ts = dt_ensure_datetime(data['ts'])
                 self.sensors[sens].temp = data['measurements']['temperature']
                 self.sensors[sens].hum = data['measurements']['humidity']
                 await self.draw_figure()
@@ -105,24 +94,20 @@ class ConditionsScreensWidget(QWidget):
                 pass
 
     async def draw_figure(self):
-        self.figure.clf()
+        self.figure.clear()
         self.figure.gca().tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False, labeltop=False, labelright=False, labelbottom=False)
         img = mpimg.imread('./oca_monitor/resources/gfx/oca_main_building.png')
         self.figure.gca().imshow(img)
         text = ''
         for s,sens in self.sensors.items():
-
             if int(sens.x)+int(sens.y)!=0:
-                #print(s)
                 if sens.temp != "Undef":
                     if sens.name_to_display != '':
                         self.figure.gca().text(int(sens.x),int(sens.y)-90,sens.name_to_display,backgroundcolor='lightgreen',color='red',fontsize='large')
                     self.figure.gca().text(int(sens.x),int(sens.y)-20,str(int(sens.temp))+'$^{\circ} C$',backgroundcolor='lightgreen',color='red',fontsize='large')
                 if sens.hum != "Undef":
                     self.figure.gca().text(int(sens.x),int(sens.y)+50,str(int(sens.hum))+'%',backgroundcolor='lightgreen',color='red',fontsize='large')
-
             else:
-                
                 if sens.temp != "Undef":
                     text = text+'\n'+sens.name_to_display+'\t'
                     text = text+str(int(sens.temp))+'C\t'
@@ -130,52 +115,29 @@ class ConditionsScreensWidget(QWidget):
                    text = text + str(int(sens.hum))+'%'
 
         self.label_conditions.setText(text)
-
-
         self.canvas.draw()
-        # QTimer.singleShot(10000, self.draw_figure)
-
-        
-        
 
     async def reader_loop_water(self):
         msg = Messenger()
-        #try:
-        if True:
-            # We want the data from the midnight of yesterday
 
-            rdr = msg.get_reader(
-                self.subject_water,
-                deliver_policy='last'
-            )
-            logger.info(f"Subscribed to {self.subject_water}")
+        rdr = msg.get_reader(
+            self.subject_water,
+            deliver_policy='last'
+        )
+        logger.info(f"Subscribed to {self.subject_water}")
 
-            
-            async for data, meta in rdr:
-                
-                if True:
-                    # if we crossed the midnight, we want to copy today's data to yesterday's and start today from scratch
-                    
-                    self.ts = dt_ensure_datetime(data['ts'])
-                    measurement = data['measurements']
-                    self.water_level = measurement['water_level']
-                    logger.debug(f"Measured water level {self.water_level}")
-                    try:
-            
-                        self.label_water.setText('Water '+str(self.water_level)+ ' litres')
-                    except:
-                        self.label_water.setText('No data')
-        #except:
-        #    pass
-
-    
+        async for data, meta in rdr:
+            try:
+                self.label_water.setText('Water '+str(data['measurements']['water_level'])+ ' litres')
+            except (LookupError, ValueError, TypeError):
+                self.label_water.setText('No data')
 
     async def reader_loop_energy(self):
         msg = Messenger()
 
         # We want the data from the midnight of yesterday
         today_midnight = datetime.datetime.combine(datetime.date.today(), datetime.time(0))
-        yesterday_midnight = today_midnight - datetime.timedelta(days=1)
+        # yesterday_midnight = today_midnight - datetime.timedelta(days=1)
 
         rdr = msg.get_reader(
             self.subject_energy,
@@ -184,47 +146,17 @@ class ConditionsScreensWidget(QWidget):
         )
         logger.info(f"Subscribed to {self.subject_energy}")
 
-        sample_measurement = {
-                "state_of_charge": 100,
-                "pv_power": 0,
-                "battery charge": 0,
-                "battery_discharge": 0,
-        }
+        # sample_measurement = {
+        #         "state_of_charge": 100,
+        #         "pv_power": 0,
+        #         "battery charge": 0,
+        #         "battery_discharge": 0,
+        # }
         async for data, meta in rdr:
             try:
-            #if True:
-                # if we crossed the midnight, we want to copy today's data to yesterday's and start today from scratch
-                now = datetime.datetime.now()
-                '''if now.date() > today_midnight.date():
-                    logger.info("Crossed the midnight, resetting the data")
-                    yesterday_midnight = today_midnight
-                    today_midnight = datetime.datetime.combine(now.date(), datetime.time(0))
-                    self.ln_yesterday_wind.set_data(
-                        self.ln_today_wind.get_xdata(),
-                        self.ln_today_wind.get_ydata()
-                    )
-                    self.ln_today_wind.set_data([], [])
-
-                    self.ln_yesterday_temp.set_data(
-                        self.ln_today_temp.get_xdata(),
-                        self.ln_today_temp.get_ydata()
-                    )
-                    self.ln_today_temp.set_data([], [])
-
-                    self.ln_yesterday_hum.set_data(
-                        self.ln_today_hum.get_xdata(),
-                        self.ln_today_hum.get_ydata()
-                    )
-                    self.ln_today_hum.set_data([], [])
-
-                    self.ln_yesterday_pres.set_data(
-                        self.ln_today_pres.get_xdata(),
-                        self.ln_today_pres.get_ydata()
-                    )
-                    self.ln_today_pres.set_data([], [])'''
-
+                # now = datetime.datetime.now()
                 # handle current datapoint. it has measurement timestamp in data.ts, and the measurement in data.measurement
-                ts = dt_ensure_datetime(data['ts']).astimezone()
+                # ts = dt_ensure_datetime(data['ts']).astimezone()
                 measurement = data['measurements']
                 soc = measurement['state_of_charge']
                 pv = measurement['pv_power']
@@ -234,76 +166,14 @@ class ConditionsScreensWidget(QWidget):
                 bd = measurement['battery_discharge']
                 ec = bd + pv - bc
                 # depending on the date of the measurement, we want to add point to the yesterday or today data
-                hour = ts.hour + ts.minute / 60 + ts.second / 3600
-                '''if ts < today_midnight.astimezone():
-                    #logger.info(f'Adding point to yesterday data {wind_speed10}')
-                    self.ln_yesterday_wind.set_data(
-                        list(self.ln_yesterday_wind.get_xdata()) + [hour],
-                        list(self.ln_yesterday_wind.get_ydata()) + [wind_speed10]
-                    )
-
-                    self.ln_yesterday_temp.set_data(
-                        list(self.ln_yesterday_temp.get_xdata()) + [hour],
-                        list(self.ln_yesterday_temp.get_ydata()) + [temp]
-                    )
-
-                    self.ln_yesterday_hum.set_data(
-                        list(self.ln_yesterday_hum.get_xdata()) + [hour],
-                        list(self.ln_yesterday_hum.get_ydata()) + [hum]
-                    )
-
-                    self.ln_yesterday_pres.set_data(
-                        list(self.ln_yesterday_pres.get_xdata()) + [hour],
-                        list(self.ln_yesterday_pres.get_ydata()) + [pres]
-                    )
-                else:
-                    #logger.info(f'Adding point to today data {wind_speed10}')
-                    self.ln_today_wind.set_data(
-                        list(self.ln_today_wind.get_xdata()) + [hour],
-                        list(self.ln_today_wind.get_ydata()) + [wind_speed10]
-                    )
-                    self.ln_today_temp.set_data(
-                        list(self.ln_today_temp.get_xdata()) + [hour],
-                        list(self.ln_today_temp.get_ydata()) + [temp]
-                    )
-
-                    self.ln_today_hum.set_data(
-                        list(self.ln_today_hum.get_xdata()) + [hour],
-                        list(self.ln_today_hum.get_ydata()) + [hum]
-                    )
-
-                    self.ln_today_pres.set_data(
-                        list(self.ln_today_pres.get_xdata()) + [hour],
-                        list(self.ln_today_pres.get_ydata()) + [pres]
-                    )
-                # lazy redraw
-                
-                self.ax_wind.relim()
-                self.ax_wind.autoscale_view()
-
-                self.ax_temp.relim()
-                self.ax_temp.autoscale_view()
-
-                self.ax_hum.relim()
-                self.ax_hum.autoscale_view()
-
-                self.ax_pres.relim()
-                self.ax_pres.autoscale_view()
-                self.canvas.draw_idle()'''
-            except:
+                # hour = ts.hour + ts.minute / 60 + ts.second / 3600
+            except (ValueError, TypeError, LookupError):
                 soc = 'NaN'
                 pv = 'NaN'
-                bc = 'NaN'
-                bd = 'NaN'
                 ec = 'NaN'
 
-            #try:
-            if True:
-                text = 'ENERGY:\nClusters state of charge\t'+str(soc)+' %\n' + 'Solar Power\t\t'+str(pv)+' W\n'+ 'Power consumption\t'+str(ec)+' W'
-                self.label_energy.setText(text)
-        #except:
-        #    self.label_energy.setText('No data')
-        #    self.label_energy.setText(text)
+            text = 'ENERGY:\nClusters state of charge\t'+str(soc)+' %\n' + 'Solar Power\t\t'+str(pv)+' W\n'+ 'Power consumption\t'+str(ec)+' W'
+            self.label_energy.setText(text)
                                        
 
 widget_class = ConditionsScreensWidget
