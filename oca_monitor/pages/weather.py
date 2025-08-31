fb76467b02f0abd01ff84138126613789eeb84aa
MMiirrkk <cot@wp.pl>
2025-08-31 13:56:09 +0200

    weather adjust 1

--- oca_monitor/pages/weather.py
+++ oca_monitor/pages/weather.py
@@ -82,13 +82,13 @@ class WeatherDataWidget(QWidget):
             self.ax_temp = self.figure.add_subplot(222)
             self.ax_hum = self.figure.add_subplot(223)
             self.ax_pres = self.figure.add_subplot(224)
-            self.figure.subplots_adjust(wspace=0.3,hspace=0.6)
+            self.figure.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0.3,hspace=0.6)
         else:
             self.ax_wind = self.figure.add_subplot(411)
             self.ax_temp = self.figure.add_subplot(412)
             self.ax_hum = self.figure.add_subplot(413)
             self.ax_pres = self.figure.add_subplot(414)
-            self.figure.subplots_adjust(wspace=0.3,hspace=0.6)
+            self.figure.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0.3,hspace=0.6)
 
         self.ax_wind.tick_params(axis='both', which='both', labelsize=13)
         self.ax_temp.tick_params(axis='both', which='both', labelsize=13)
