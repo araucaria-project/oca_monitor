a86e16d5a19b1992a9b9a4c90af6f0e60692aeb6
MMiirrkk <cot@wp.pl>
2025-12-22 11:31:52 +0100

    refactor water pump 1g

--- oca_monitor/pages/touch_buttons_wbedroom.py
+++ oca_monitor/pages/touch_buttons_wbedroom.py
@@ -43,14 +43,15 @@ class WaterPump:
         self.button = QCheckBox()
         self.button.setStyleSheet("QCheckBox::indicator{width: 170px; height:170px;} QCheckBox::indicator:checked {image: url(./Icons/hot_water_on.png)} QCheckBox::indicator:unchecked {image: url(./Icons/hot_water_off.png)}")
         self.button.setChecked(False)
+        self.button.clicked.connect(self.button_pressed)
 
     @property
     def url(self) -> str:
         return f'http://{self.ip}/state'
 
-    @asyncSlot()
-    async def connect(self):
-        self.button.clicked.connect(self.button_pressed)
+    # @asyncSlot()
+    # async def connect(self):
+    #     self.button.clicked.connect(self.button_pressed)
 
     @asyncSlot()
     async def button_pressed(self) -> None:
@@ -178,7 +179,6 @@ class TouchButtonsWBedroom(QWidget):
             sort_reverse=True
         )
         await display.display_init()
-        await self.water_pump.connect()
 
     # @asyncSlot()
     # async def water_button_pressed(self,wylacz=False):
