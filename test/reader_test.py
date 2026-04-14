4ffd7c241bb59ec895da05417c5c04c505e79848
MMiirrkk <cot@wp.pl>
2026-04-14 19:03:10 +0200

    add test 1

---
+++ test/reader_test.py
@@ -0,0 +1,21 @@
+import asyncio
+import datetime
+
+import serverish
+from serverish.messenger import Messenger
+
+
+async def start():
+    msg = Messenger()
+    await msg.open("nats.oca.lan", 4222, wait=3)
+    ts = datetime.datetime.now(datetime.timezone.utc)
+    rdr = msg.get_reader(
+        "",
+        deliver_policy='by_start_time',
+        opt_start_time=ts
+    )
+    async for data, meta in rdr:
+        print(data)
+        print(meta)
+
+asyncio.run(start())
