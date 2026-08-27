5de405fbc3f19a0d1a4eb0a55a391c2d68824625
MMiirrkk <cot@wp.pl>
2026-08-27 12:51:03 +0200

    allsky fix 1a

--- oca_monitor/pages/allsky_animation_mpl.py
+++ oca_monitor/pages/allsky_animation_mpl.py
@@ -13,57 +13,54 @@ from qasync import asyncSlot
 
 from oca_monitor.image_display import ImageDisplay
 
-
 logger = logging.getLogger(__name__.rsplit('.')[-1])
 
 
 class AllskyAnimationMplWidget(QWidget):
-    def __init__(self, main_window, allsky_dir='/data/misc/allsky/', vertical_screen=False,
-                 max_age_sec: float = 300, **kwargs):
+    def __init__(self, main_window, allsky_dir='/data/misc/allsky/', vertical_screen=False, **kwargs):
         super().__init__()
         self.main_window = main_window
         self.dir = allsky_dir
         self.freq = 500
         self.vertical = bool(vertical_screen)
         self.counter = 0
-        self.max_age_sec = float(max_age_sec)
         self.initUI()
         logger.info(f"AllskyAnimationMplWidget init setup done")
-        
+
     def initUI(self):
         self.layout = QVBoxLayout(self)
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
-        self.layout.addWidget(self.canvas,1)
+        self.layout.addWidget(self.canvas, 1)
         QTimer.singleShot(0, self.async_init)
         # logger.info(f"AllskyAnimationMplWidget UI setup done")
 
-    def calc_tel_xy(self,x_0,y_0,alt,az):
+    def calc_tel_xy(self, x_0, y_0, alt, az):
         pi = 3.14159
-        az = (270.-az) * pi / 180.
-        alt = (90-alt) * 520/90.
+        az = (270. - az) * pi / 180.
+        alt = (90 - alt) * 520 / 90.
 
         x = x_0 - alt * math.cos(az)
         y = y_0 + alt * math.sin(az)
-        return x,y
+        return x, y
 
-    def calc_wind_arrow(self,x_0,y_0,r,r_prim):
+    def calc_wind_arrow(self, x_0, y_0, r, r_prim):
         pi = 3.14159
         try:
             winddir = self.main_window.winddir
-            x = x_0-r*math.cos((270.-winddir)*pi/180.)
-            y = y_0+r*math.sin((270.-winddir)*pi/180.)
-            x_prim = x_0-r_prim*math.cos((270.-winddir)*pi/180.)
-            y_prim = y_0+r_prim*math.sin((270.-winddir)*pi/180.)
+            x = x_0 - r * math.cos((270. - winddir) * pi / 180.)
+            y = y_0 + r * math.sin((270. - winddir) * pi / 180.)
+            x_prim = x_0 - r_prim * math.cos((270. - winddir) * pi / 180.)
+            y_prim = y_0 + r_prim * math.sin((270. - winddir) * pi / 180.)
 
         except:
             winddir = 180.
-            x = x_0-r*math.cos((270.-winddir)*pi/180.)
-            y = y_0+r*math.sin((270.-winddir)*pi/180.)
-            x_prim = x_0-r_prim*math.cos((270.-winddir)*pi/180.)
-            y_prim = y_0+r_prim*math.sin((270.-winddir)*pi/180.)
+            x = x_0 - r * math.cos((270. - winddir) * pi / 180.)
+            y = y_0 + r * math.sin((270. - winddir) * pi / 180.)
+            x_prim = x_0 - r_prim * math.cos((270. - winddir) * pi / 180.)
+            y_prim = y_0 + r_prim * math.sin((270. - winddir) * pi / 180.)
 
-        return x,y,x_prim-x,y_prim-y
+        return x, y, x_prim - x, y_prim - y
 
     # def update(self):
     #     lista = os.popen('ls -tr '+self.dir+'lastimage*.jpg').read().split('\n')[:-1]
@@ -158,23 +155,6 @@ class AllskyAnimationMplWidget(QWidget):
         self.layout.insertWidget(0, object_to_display)
         self.canvas.draw()
 
-    async def show_stale(self, reason: str) -> None:
-        """Replace the canvas with a red-on-black diagnostic so observers do not
-        mistake yesterday's images for current sky."""
-        figure = Figure(facecolor='black')
-        ax = figure.add_subplot(111)
-        ax.set_facecolor('black')
-        ax.text(
-            0.5, 0.5,
-            f"ALLSKY UNAVAILABLE\n\n{reason}",
-            ha='center', va='center',
-            fontsize=22, color='red', fontweight='bold',
-            transform=ax.transAxes,
-        )
-        ax.axis('off')
-        canvas = FigureCanvas(figure)
-        await self.image_display(canvas)
-
     @asyncSlot()
     async def async_init(self):
         logger.info('Starting allsky display.')
@@ -182,13 +162,9 @@ class AllskyAnimationMplWidget(QWidget):
             name='allsky', images_dir=self.dir, image_display_clb=self.image_display,
             image_instance_clb=self.image_instance, images_prefix='lastimage',
             image_cascade_sec=0.75, image_pause_sec=1.25, refresh_list_sec=10, mode='update_files',
-            sort_reverse=True,
-            max_age_sec=self.max_age_sec, on_stale_clb=self.show_stale,
+            sort_reverse=True
         )
         await display.display_init()
 
+
 widget_class = AllskyAnimationMplWidget
-
-
-
-
